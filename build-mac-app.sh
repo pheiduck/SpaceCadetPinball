de062367644e4116c9bcbf2148ba62e532d2346c
Philip H. <47042125+pheiduck@users.noreply.github.com>
2025-08-03 20:27:26 +0200

    Update build-mac-app.sh

--- build-mac-app.sh
+++ build-mac-app.sh
@@ -6,20 +6,20 @@ set -xe
 mkdir -p Libs
 cd Libs
 
-# === SDL2 ===
-sdl_version='2.32.8'
-sdl_filename="SDL2-$sdl_version.dmg"
+# === SDL3 ===
+sdl_version='3.2.18'
+sdl_filename="SDL3-$sdl_version.dmg"
 sdl_url="https://github.com/libsdl-org/SDL/releases/download/release-$sdl_version/$sdl_filename"
 
 if [ ! -f "$sdl_filename" ]; then
   curl -sSf -L -O "$sdl_url"
 fi
 
-echo "de07f71cc85cd8909c977e105c4f2ca419abd09b981c347e003592186caf6fa0  $sdl_filename" | shasum -a 256 -c
+echo "sha256:137029b3b4d76df95350bb1d5336d5a593e4e1eaf9faf413abee09da6c5ec037  $sdl_filename" | shasum -a 256 -c
 
 hdiutil attach "$sdl_filename" -quiet
-cp -a /Volumes/SDL2/SDL2.framework .
-hdiutil detach /Volumes/SDL2
+cp -a /Volumes/SDL3/SDL3.framework .
+hdiutil detach /Volumes/SDL3
 
 # === SDL2_mixer ===
 sdl_mixer_version='2.8.1'
@@ -51,7 +51,7 @@ mkdir -p SpaceCadetPinball.app/Contents/Frameworks
 
 cp -a Platform/macOS/Info.plist SpaceCadetPinball.app/Contents/
 cp -a Platform/macOS/SpaceCadetPinball.icns SpaceCadetPinball.app/Contents/Resources/
-cp -a Libs/SDL2.framework SpaceCadetPinball.app/Contents/Frameworks/
+cp -a Libs/SDL3.framework SpaceCadetPinball.app/Contents/Frameworks/
 cp -a Libs/SDL2_mixer.framework SpaceCadetPinball.app/Contents/Frameworks/
 cp -a bin/SpaceCadetPinball SpaceCadetPinball.app/Contents/MacOS/
 
