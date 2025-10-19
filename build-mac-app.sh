60a69c57222c39bb02a887b6d2d03fd6b9b6dc62
Philip H. <47042125+pheiduck@users.noreply.github.com>
2025-10-19 11:14:04 +0200

    Release 2.2.6

--- build-mac-app.sh
+++ build-mac-app.sh
@@ -43,7 +43,7 @@ cmake -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=bin .
 cmake --build .
 
 # === Prepare .app Bundle ===
-sw_version='2.2.5'
+sw_version='2.2.6'
 
 mkdir -p SpaceCadetPinball.app/Contents/MacOS
 mkdir -p SpaceCadetPinball.app/Contents/Resources
